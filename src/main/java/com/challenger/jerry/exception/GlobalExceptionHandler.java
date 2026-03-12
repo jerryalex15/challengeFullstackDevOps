9b0fbb1b81008e578f0c57f6a43ab2b708272680
jerryalex15 <jerryrazafindraibe@gmail.com>
2026-03-12 13:39:14 +0100

    add unit test to Exception

--- src/main/java/com/challenger/jerry/exception/GlobalExceptionHandler.java
+++ src/main/java/com/challenger/jerry/exception/GlobalExceptionHandler.java
@@ -12,9 +12,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleInvalidRefreshToken(InvalidRefreshTokenException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
 }
